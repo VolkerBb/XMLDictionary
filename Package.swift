d4d96df951e301d66294eb0497d8376343ab1af5
Volker Bublitz <v.bublitz@apps2go.de>
2017-02-24 09:34:14 +0100

    swift package structure changed

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "XMLDictionary",
-    targets: [
-        Target(name: "XMLDictionary", dependencies: [])
-    ],
-    dependencies: [],
-    exclude: ["Tests", "XMLDictionary"]
+    name: "XMLDictionary"
 )
