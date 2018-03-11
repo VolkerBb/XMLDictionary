94f752f3d198de644865fd40b4632d3d71c1e731
Volker Bublitz <v.bublitz@apps2go.de>
2018-03-11 13:28:46 +0100

    prepared test cases

--- Tests/XMLDictionaryTests/XMLSimpleTest.swift
+++ Tests/XMLDictionaryTests/XMLSimpleTest.swift
@@ -7,74 +7,111 @@
 //
 
 import XCTest
+@testable import XMLDictionary
 
 class XMLSimpleTest: XCTestCase {
 
     var bundle:Bundle?
+    var parsedDictionary:XMLDictionary?
     
     override func setUp() {
         super.setUp()
-        self.bundle = Bundle(for: XMLSimpleTest.self)
+        #if os(Linux)
+            // copied from https://github.com/IBM-Swift/Bridging/blob/master/Sources/Bridging/FoundationAdapterLinux.swift
+            //
+            // Bundle(for:) is not yet implemented on Linux
+            //TODO remove this ifdef once Bundle(for:) is implemented
+            // issue https://bugs.swift.org/browse/SR-953
+            // meanwhile return a Bundle whose resource path points to /Resources directory
+            //     inside the resourcePath of Bundle.main (e.g. .build/debug/Resources)
+            bundle = Bundle(path: (Bundle.main.resourcePath ?? ".") + "/Resources") ?? Bundle.main
+        #else
+            bundle = Bundle(for: Swift.type(of: self))
+        #endif
+        NSLog("bundle path: %@", bundle?.bundlePath ?? "none")
+        parsedDictionary = parse(filePath: bundle?.path(forResource: "example", ofType: "xml"))
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
-    func parse(url: URL) {
+    func parse(url: URL) -> XMLDictionary? {
         if let xmlString = try? String(contentsOf: url, encoding: .utf8) {
-            NSLog("string: %@", [xmlString])
-            let xmlDictionary = XMLDictionary.dictionaryWithXMLString(xmlString: xmlString)
-            NSLog("dictionary: %@", [xmlDictionary])
+            return XMLDictionary.dictionaryWithXMLString(xmlString: xmlString)
         }
+        return nil
+    }
+    
+    func parse(filePath: String?) -> XMLDictionary? {
+        guard let p = filePath else {
+            return nil
+        }
+        return XMLDictionary.dictionaryWithXMLFile(xmlFilePath: p)
+    }
+    
+    func testBaseVars() {
+        XCTAssertNotNil(bundle, "bundle is nil")
+        XCTAssertNotNil(parsedDictionary, "parsed dictionary is nil")
     }
     
     func testExample() {
-        if let path = bundle?.path(forResource: "example", ofType: "xml") {
-            guard let xmlDictionary = XMLDictionary.dictionaryWithXMLFile(xmlFilePath: path) else {
-                XCTAssert(false, "parsing error")
-                return
-            }
-            NSLog("dictionary: %@", [xmlDictionary])
-            
-            let value = xmlDictionary.value(forKeyPath: "book.0._id")
-            guard let v = value as? String else {
-                XCTAssert(false, "book not found")
-                return
-            }
-            XCTAssert(v == "bk101", "book not found")
-            
-            if let bookNode = xmlDictionary.value(forKeyPath: "book.1") as? [String : Any] {
-                let idAttribute = bookNode.attributeForKey(key: "id")
-                XCTAssert(idAttribute == "bk102", "book id attribute not found")
-            }
-            
-            let dict = xmlDictionary.dictionaryValue(forKeyPath: "book")
-            XCTAssert((dict!["_id"] as! String) == "bk101", "book not found")
-            
-            let array = xmlDictionary.arrayValue(forKeyPath: "book")
-            XCTAssert(((array![1] as! [String : Any])["_id"] as! String) == "bk102", "book 2 not found")
-            
-            let str = xmlDictionary.stringValue(forKeyPath: "book.1")
-            XCTAssert(str! == "An extra text content of this book.", "book 2 additional text not found")
-            
-            let dict2 = xmlDictionary.dictionaryValue(forKeyPath: "book.1")
-            XCTAssert((dict2!["_id"] as! String) == "bk102", "book not found")
-            
-            let valueList = [xmlDictionary].map({ (($0["book"] as! [Any])[0] as! [String:Any])["_id"] as! String })
-            XCTAssert((valueList.first ?? "") == "bk101", "book id not found")
+        guard let xmlDictionary = parsedDictionary else {
+            XCTAssert(false, "parsing error")
+            return
         }
-
+        
+        let value = xmlDictionary.value(forKeyPath: "book.0._id")
+        guard let v = value as? String else {
+            XCTAssert(false, "book not found")
+            return
+        }
+        XCTAssert(v == "bk101", "book not found")
+        
+        if let bookNode = xmlDictionary.value(forKeyPath: "book.1") as? [String : Any] {
+            let idAttribute = bookNode.attributeForKey(key: "id")
+            XCTAssert(idAttribute == "bk102", "book id attribute not found")
+        }
+        
+        let dict = xmlDictionary.dictionaryValue(forKeyPath: "book")
+        XCTAssert((dict!["_id"] as! String) == "bk101", "book not found")
+        
+        let array = xmlDictionary.arrayValue(forKeyPath: "book")
+        XCTAssert(((array![1] as! [String : Any])["_id"] as! String) == "bk102", "book 2 not found")
+        
+        let str = xmlDictionary.stringValue(forKeyPath: "book.1")
+        XCTAssert(str! == "An extra text content of this book.", "book 2 additional text not found")
+        
+        let dict2 = xmlDictionary.dictionaryValue(forKeyPath: "book.1")
+        XCTAssert((dict2!["_id"] as! String) == "bk102", "book not found")
+        
+        let valueList = [xmlDictionary].map({ (($0["book"] as! [Any])[0] as! [String:Any])["_id"] as! String })
+        XCTAssert((valueList.first ?? "") == "bk101", "book id not found")
+    }
+    
+    func testURLParsing() {
         if let url2 = URL(string: "http://www.ibiblio.org/xml/examples/shakespeare/all_well.xml") {
-            self.parse(url: url2)
+            XCTAssertNotNil(self.parse(url: url2), "Parsing of http://www.ibiblio.org/xml/examples/shakespeare/all_well.xml failed")
         }
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testParsingPerformance() {
+        NSLog("Measuring xml file to XMLDictionary")
         self.measure {
-            // Put the code you want to measure the time of here.
+            if let path = self.bundle?.path(forResource: "example", ofType: "xml") {
+                let _ = XMLDictionary.dictionaryWithXMLFile(xmlFilePath: path)
+            }
+        }
+    }
+    
+    func testEncodingPerformance() {
+        guard let xmlDictionary = parsedDictionary else {
+            XCTAssert(false, "parsing error")
+            return
+        }
+        NSLog("Measuring XMLDictionary to xmlString")
+        self.measure {
+            let _ = xmlDictionary.xmlString()
         }
     }
 
---
+++ makefile
@@ -0,0 +1,52 @@
+# Copyright Vadim Eisenberg 2017
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+UNAME = ${shell uname}
+
+# set EXECUTABLE_DIRECTORY according to your specific environment
+# run swift build and see where the output executable is created
+
+ifeq ($(UNAME), Darwin)
+PLATFORM = x86_64-apple-macosx10.10
+EXECUTABLE_DIRECTORY = ./.build/${PLATFORM}/debug
+TEST_RESOURCES_DIRECTORY = ./.build/${PLATFORM}/debug/XMLDictionaryPackageTests.xctest/Contents/Resources
+else ifeq ($(UNAME), Linux)
+PLATFORM = x86_64-unknown-linux
+EXECUTABLE_DIRECTORY = ./.build/${PLATFORM}/debug
+TEST_RESOURCES_DIRECTORY = ${EXECUTABLE_DIRECTORY}
+endif
+
+RUN_RESOURCES_DIRECTORY = ${EXECUTABLE_DIRECTORY}
+
+build: copyRunResources
+	swift build
+
+copyRunResources:
+	mkdir -p ${RUN_RESOURCES_DIRECTORY}
+	cp Resources/* ${RUN_RESOURCES_DIRECTORY}
+
+copyTestResources:
+	mkdir -p ${TEST_RESOURCES_DIRECTORY}
+	cp Resources/* ${TEST_RESOURCES_DIRECTORY}
+
+run: build
+	${EXECUTABLE_DIRECTORY}/XMLDictionary
+
+test: copyTestResources
+	swift test
+
+clean:
+	rm -rf .build
+
+.PHONY: run build test copyRunResources copyTestResources clean
